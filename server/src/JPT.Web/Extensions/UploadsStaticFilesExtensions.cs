8fb3da76281c25febe7f2f284c9a041af3ecc9bd
tuantai0708 <tai.bang157@gmail.com>
2025-08-12 19:42:09 +0700

    feat(user): To add static file Uploads folder

--- server/src/JPT.Web/Extensions/UploadsStaticFilesExtensions.cs
+++ server/src/JPT.Web/Extensions/UploadsStaticFilesExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace JPT.Web.Extensions;
+﻿using Microsoft.Extensions.FileProviders;
+
+namespace JPT.Web.Extensions;
 
 internal static class UploadsStaticFilesExtensions
 {
@@ -6,5 +8,11 @@ internal static class UploadsStaticFilesExtensions
     {
         var uploadsAbs = Path.Combine(app.Environment.ContentRootPath, "Uploads");
         Directory.CreateDirectory(uploadsAbs);
+        
+        app.UseStaticFiles(new StaticFileOptions
+        {
+            FileProvider = new PhysicalFileProvider(uploadsAbs),
+            RequestPath  = "/Uploads"
+        });
     }
 }
